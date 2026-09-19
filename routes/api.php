94450161bce484f7be45badf300d9ca4f9dbc198
Poupson <fonkamchristian1@gmail.com>
2026-09-19 17:41:59 +0000

    Correction balise PHP manquante dans web.php et nettoyage imports dans api.php

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,14 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommandeController;
 use App\Http\Controllers\StatistiqueController;
 use App\Http\Controllers\SousCategorieController;
 use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 // Routes publiques (authentification)
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -19,21 +21,18 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
 
-   
     Route::apiResource('articles', ArticleController::class);
-    
     Route::apiResource('commandes', CommandeController::class);
     Route::apiResource('sous-categories', SousCategorieController::class);
-    
+
     Route::get('/statistiques', [StatistiqueController::class, 'index']);
     Route::apiResource('users', UserController::class)->except(['show']);
     Route::get('/statistiques/evolution', [StatistiqueController::class, 'evolution']);
 });
-use App\Models\User;
 
 Route::get('/check-users', function () {
     return response()->json([
         'count' => User::count(),
-        'emails' => User::all()->pluck('email')
+        'emails' => User::all()->pluck('email'),
     ]);
 });
